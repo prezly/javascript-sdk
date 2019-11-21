87245403658e3a16797c3e07a5599fe3c5c5a881
Marko Grešak <marko@gresak.io>
2019-11-21 17:28:42 +0100

    Import version from package.json using require workaround

--- src/Sdk/constants.ts
+++ src/Sdk/constants.ts
@@ -1,3 +1,5 @@
-const packageVersion = '1.0';
+// When using `import`, TS complains because `package.json` is outside the
+// `rootDir`, but it works with `require`.
+const { version: packageVersion, repository } = require('../../package.json');
 
-export const DEFAULT_USER_AGENT = `prezly-javascript-sdk/${packageVersion} (+https://github.com/prezly/javascript-sdk)`;
+export const DEFAULT_USER_AGENT = `prezly-javascript-sdk/${packageVersion} (+${repository.url})`;
