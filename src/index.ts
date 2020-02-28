70767f6f0d780c9825fe7e7deef62d55c3c2a465
Ivan Voskoboinyk <ivan@prezly.com>
2020-02-28 15:15:04 +0200

    Export all types & request/response interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export { default } from './Sdk';
+export { default } from './Sdk/Sdk';
+export {
+    Contact,
+    Coverage,
+    Entity,
+    Pagination,
+    RoomRef,
+    Story,
+} from './types'
+export {
+    Options as ClientOptions,
+} from './Sdk/types'
+export {
+    CoverageCreateRequest,
+    CoverageListResponse,
+    CoverageSearchOptions,
+    CoverageUpdateRequest,
+} from './Sdk/Coverage/types'
