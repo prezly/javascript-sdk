dcb3937e84dcabb077279b1ad2f76f2cf3ebaa28
Ivan Voskoboinyk <ivan@prezly.com>
2021-01-20 18:53:18 +0200

    Do export all Newsroom Contacts SDK types
    Along with other types that were not imported before.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 export {
     default,
     ClientOptions,
+    // Coverage SDK
     CoverageCreateRequest,
     CoverageListResponse,
     CoverageSearchOptions,
     CoverageUpdateRequest,
+    // Newsrooms SDK
+    NewsroomCreateRequest,
+    NewsroomListRequest,
+    NewsroomListResponse,
+    NewsroomSearchRequest,
+    NewsroomUpdateRequest,
+    // Newsroom Categories SDK
+    NewsroomCategoriesListOptions,
+    NewsroomCategoryCreateRequest,
+    NewsroomCategoryUpdateRequest,
+    // Newsroom Contacts SDK
+    NewsroomContactsListRequestOptions,
+    NewsroomContactsSearchRequestOptions,
+    NewsroomContactCreateRequest,
+    NewsroomContactUpdateRequest,
+    // Stories SDK
+    StoriesListRequest,
+    StoriesListResponse,
+    StoriesSearchRequest,
+    StoryCreateRequest,
+    HtmlStoryCreateRequest,
+    SlateStoryCreateRequest,
 } from './Sdk';
 export {
     Contact,
