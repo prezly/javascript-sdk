2a03431f5b7886001e235641780ff4e6f64deb98
marek <marko.ognjenovic@prezly.com>
2021-01-26 13:40:06 +0100

    Spcify optional parameters in NewsroomSubscriptionCreateRequest

--- src/Sdk/NewsroomSubscriptions/types.ts
+++ src/Sdk/NewsroomSubscriptions/types.ts
@@ -2,25 +2,37 @@ import { Contact, UploadcareImage } from '../../types';
 
 export interface NewsroomSubscriptionCreateRequest {
     email_address: string;
-    locale?: string | null;
+    locale?: string;
+    url?: string | null;
+    ip_address?: string | null;
+    visitor_uid?: string | null;
+    session_uid?: string | null;
     contact?: {
         contact_type: Contact['contact_type'];
         company_name: Contact['company_name'];
-        first_name: Contact['first_name'];
-        last_name: Contact['last_name'];
-        gender: Contact['gender'];
-        function_name: Contact['function_name'];
-        organisations: string[];
-        avatar_image: UploadcareImage | null;
-        languages: string[];
-        emails: string[];
-        phone_nubmers: Contact['phone_numbers'];
-        urls: Contact['urls'];
-        social_accounts: Contact['social'][];
-        tags: Contact['tags'];
-        periodicity: Contact['periodicity'];
-        medium_types: Contact['medium_types'];
-        salutation: string;
-        address: Contact['address'];
+        first_name?: Contact['first_name'];
+        last_name?: Contact['last_name'];
+        gender?: Contact['gender'];
+        function_name?: Contact['function_name'];
+        organisations?: string[];
+        avatar_image?: UploadcareImage | null;
+        languages?: string[];
+        emails?: Contact['emails'];
+        phone_numbers?: Contact['phone_numbers'];
+        urls?: Contact['urls'];
+        social_accounts?: Contact['social'][];
+        tags?: Contact['tags'];
+        periodicity?: Contact['periodicity'];
+        medium_types?: Contact['medium_types'];
+        salutation?: Contact['salutation'];
+        address?: {
+            street?: Contact['address']['street'];
+            number?: Contact['address']['number'];
+            box?: Contact['address']['box'];
+            zip?: Contact['address']['zip'];
+            city?: Contact['address']['city'];
+            region?: Contact['address']['region'];
+            country?: Contact['address']['country'];
+        };
     };
 }
