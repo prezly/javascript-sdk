194ddd498fecc50bad4323f3693994ff034a969a
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-07-06 17:39:48 +0200

    Add NewsroomThemes SDK class

---
+++ src/Sdk/NewsroomThemes/NewsroomThemes.ts
@@ -0,0 +1,21 @@
+import { Newsroom, NewsroomTheme, NewsroomThemePreset } from '../../types';
+
+import routing from '../routing';
+import ApiClient from '../ApiClient';
+
+type NewsroomId = Newsroom['uuid'] | Newsroom['id'];
+type NewsroomThemeId = NewsroomTheme['id'];
+
+export default class NewsroomThemes {
+    private readonly apiClient: ApiClient;
+
+    constructor({ apiClient }: { apiClient: ApiClient }) {
+        this.apiClient = apiClient;
+    }
+
+    public async list(newsroomId: NewsroomId): Promise<NewsroomThemePreset[]> {
+        const url = routing.newsroomThemesUrl.replace(':newsroom_id', String(newsroomId));
+        const response = await this.apiClient.get<{ themes: NewsroomThemePreset[] }>(url);
+        return response.payload.themes;
+    }
+}
--- src/Sdk/Sdk.ts
+++ src/Sdk/Sdk.ts
@@ -6,6 +6,7 @@ import Newsrooms from './Newsrooms';
 import NewsroomCategories from './NewsroomCategories';
 import NewsroomContacts from './NewsroomContacts';
 import NewsroomLanguages from './NewsroomLanguages';
+import NewsroomThemes from './NewsroomThemes';
 import NewsroomWebhooks from './NewsroomWebhooks';
 
 const BASE_URL = 'https://api.prezly.com';
@@ -16,6 +17,7 @@ export default class Sdk {
     public newsroomCategories: NewsroomCategories;
     public newsroomContacts: NewsroomContacts;
     public newsroomLanguages: NewsroomLanguages;
+    public newsroomThemes: NewsroomThemes;
     public newsroomWebhooks: NewsroomWebhooks;
     public stories: Stories;
 
@@ -31,6 +33,7 @@ export default class Sdk {
         this.newsroomCategories = new NewsroomCategories({ apiClient });
         this.newsroomContacts = new NewsroomContacts({ apiClient });
         this.newsroomLanguages = new NewsroomLanguages({ apiClient });
+        this.newsroomThemes = new NewsroomThemes({ apiClient });
         this.newsroomWebhooks = new NewsroomWebhooks({ apiClient });
         this.stories = new Stories({ apiClient });
     }
