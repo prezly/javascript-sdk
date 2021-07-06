c586d74cc3aff33e66bfac9c741f44947ab9d489
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-07-06 17:33:59 +0200

    Add NewsroomThemePreset and NewsroomTheme interfaces

---
+++ src/types/NewsroomThemePreset.ts
@@ -0,0 +1,16 @@
+import { UploadcareImage } from './common';
+
+export interface NewsroomTheme {
+    id: string;
+    name: string;
+    description: string;
+    features: string[];
+    preview_image: UploadcareImage;
+}
+
+export default interface NewsroomThemePreset {
+    theme: NewsroomTheme;
+    editable_settings: Record<string, string>;
+    settings: Record<string, any>;
+    is_active: boolean;
+}
