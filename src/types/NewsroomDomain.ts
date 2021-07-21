9cab9611f0b98ae0e7eac94298d05d9548bfb180
marek <marko.ognjenovic@prezly.com>
2021-07-21 11:35:30 +0200

    Add missing reverse_proxy enum value to NewsroomDomain.type

--- src/types/NewsroomDomain.ts
+++ src/types/NewsroomDomain.ts
@@ -2,7 +2,7 @@ import DnsConfigurationInstruction from './DnsConfigurationInstruction';
 
 export default interface NewsroomDomain {
     domain_name: string;
-    type: 'prezly' | 'custom';
+    type: 'prezly' | 'custom' | 'reverse_proxy';
     status: 'active' | 'inactive' | 'redirect';
     instructions: DnsConfigurationInstruction[];
 }
