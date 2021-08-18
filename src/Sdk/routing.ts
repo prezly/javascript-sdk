ed2438f345c1e08b0f21b5df25d52174e69cf0ff
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2021-08-18 11:29:28 +0200

    Add licenseUnsubscribeUrl in alphabetical order to routing.ts

--- src/Sdk/routing.ts
+++ src/Sdk/routing.ts
@@ -1,5 +1,6 @@
 const routing = {
     coverageUrl: '/v2/coverage',
+    licenseUnsubscribeUrl: '/v2/unsubscribe',
     newsroomsUrl: '/v2/newsrooms',
     newsroomCategoriesUrl: '/v2/newsrooms/:newsroom_id/categories',
     newsroomContactsUrl: '/v2/newsrooms/:newsroom_id/contacts',
@@ -8,7 +9,6 @@ const routing = {
     newsroomSubscriptionsUrl: '/v2/newsrooms/:newsroom_id/subscriptions',
     newsroomSubscribeUrl: '/v2/newsrooms/:newsroom_id/subscribe',
     newsroomUnsubscribeUrl: '/v2/newsrooms/:newsroom_id/unsubscribe',
-    licenseUnsubscribeUrl: '/v2/unsubscribe',
     newsroomThemesUrl: '/v2/newsrooms/:newsroom_id/themes',
     newsroomWebhooksUrl: '/v2/newsrooms/:newsroom_id/webhooks',
     newsroomDomainsUrl: '/v2/newsrooms/:newsroom_id/domains',
