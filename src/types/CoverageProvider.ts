6322440013282f6d225d2794f47152138edad838
Gijs Nelissen <gijs@prezly.com>
2021-10-25 14:47:23 +0200

    Add Ammco and Mediaweb

--- src/types/CoverageProvider.ts
+++ src/types/CoverageProvider.ts
@@ -1,7 +1,9 @@
 enum CoverageProvider {
+    AMMCO = 'ammco',
     BELGA = 'belga',
     FEED = 'feed',
     KNEWIN = 'knewin',
+    MEDIAWEB = 'mediaweb',
     MELTWATER = 'meltwater',
     MANUAL = 'manual',
 }
