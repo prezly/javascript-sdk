4bdd11290c35b55a0ddf9b4053ff6c59977637f2
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-11-09 16:02:26 +0100

    Specify all features in ThemeFeature enum

--- src/types/ThemeFeature.ts
+++ src/types/ThemeFeature.ts
@@ -6,4 +6,7 @@ export enum ThemeFeature {
     MULTI_LANGUAGE = 'multi_language',
     CUSTOM_COLORS = 'custom_colors',
     OPTIONAL_PRIVACY_PORTAL = 'optional_privacy_portal',
+    CUSTOM_CSS_JAVASCRIPT = 'custom_css_javascript',
+    HOMEPAGE_CONTACTS = 'homepage_contacts',
+    SUBSCRIBE_FORM = 'subscribe_form',
 }
