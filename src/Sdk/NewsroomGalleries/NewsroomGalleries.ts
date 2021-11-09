0d8764bd9e91eed89ef996411ca92c031f197bcb
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2021-11-09 14:22:17 +0100

    Fix code style

--- src/Sdk/NewsroomGalleries/NewsroomGalleries.ts
+++ src/Sdk/NewsroomGalleries/NewsroomGalleries.ts
@@ -3,7 +3,7 @@ import { Newsroom } from '../../types';
 import routing from '../routing';
 import DeferredJobsApiClient from '../DeferredJobsApiClient';
 import { NewsroomGallery } from '../../types';
-import {NewsroomGalleriesListRequest, NewsroomGalleriesListResponse} from "./types";
+import { NewsroomGalleriesListRequest, NewsroomGalleriesListResponse } from './types';
 
 type NewsroomId = Newsroom['uuid'] | Newsroom['id'];
 
@@ -24,7 +24,10 @@ export default class NewsroomGalleries {
         return response.gallery;
     }
 
-    public async list(newsroomId: NewsroomId, payload: NewsroomGalleriesListRequest = {}): Promise<NewsroomGalleriesListResponse> {
+    public async list(
+        newsroomId: NewsroomId,
+        payload: NewsroomGalleriesListRequest = {},
+    ): Promise<NewsroomGalleriesListResponse> {
         const url = routing.newsroomGalleriesUrl.replace(':newsroom_id', String(newsroomId));
         return this.apiClient.get<NewsroomGalleriesListResponse>(url, {
             query: payload,
