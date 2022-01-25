036d5d1d24d078548adf2b5d2030c1153b9e74cf
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2022-01-25 12:13:13 +0100

    Specify create request types

--- src/Sdk/NewsroomPrivacyRequests/types.ts
+++ src/Sdk/NewsroomPrivacyRequests/types.ts
@@ -2,28 +2,28 @@ import { PrivacyRequestType } from '../../types';
 
 export interface DeletePrivacyRequestCreateRequest {
     email: string;
-    type: PrivacyRequestType;
+    type: PrivacyRequestType.DELETE;
     message: string;
     extra_message?: string;
 }
 
 export interface ExportPrivacyRequestCreateRequest {
     email: string;
-    type: PrivacyRequestType;
+    type: PrivacyRequestType.EXPORT;
     message?: string;
     extra_message?: string;
 }
 
 export interface CorrectPrivacyRequestCreateRequest {
     email: string;
-    type: PrivacyRequestType;
+    type: PrivacyRequestType.CORRECT;
     message: string;
     extra_message: string;
 }
 
 export interface OtherPrivacyRequestCreateRequest {
     email: string;
-    type: PrivacyRequestType;
+    type: PrivacyRequestType.OTHER;
     message: string;
     extra_message?: string;
 }
