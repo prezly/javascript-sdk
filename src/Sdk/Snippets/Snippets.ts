a65141ba6fb83a88df9769d8a7c1212f245a5a2d
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2022-09-20 15:37:36 +0200

    Implement snippet read API sdk

--- src/Sdk/Snippets/Snippets.ts
+++ src/Sdk/Snippets/Snippets.ts
@@ -4,11 +4,25 @@ import routing from '../routing';
 import { SnippetCreateRequest } from './types';
 import { Snippet } from '../../types/Snippet';
 
+type SnippetId = Snippet['uuid'] | Snippet['id'];
+
 export default class Snippets {
     private readonly apiClient: DeferredJobsApiClient;
 
     constructor({ apiClient }: { apiClient: DeferredJobsApiClient }) {
         this.apiClient = apiClient;
+    }
+
+    public async list(): Promise<Snippet[]> {
+        const url = routing.snippetsUrl;
+        const { snippets } = await this.apiClient.get<{ snippets: Snippet[] }>(url);
+        return snippets;
+    }
+
+    public async get(snippetId: SnippetId): Promise<Snippet> {
+        const url = routing.snippetsUrl;
+        const { snippet } = await this.apiClient.get<{ snippet: Snippet }>(`${url}/${snippetId}`);
+        return snippet;
     }
 
     public async create(payload: SnippetCreateRequest): Promise<Snippet> {
