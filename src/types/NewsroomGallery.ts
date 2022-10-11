96b3c7b4dbbdabee08eb3d16dc80fec019c63695
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2022-10-11 11:10:18 +0200

    Define additional gallery API entity properties
    - name
    - description
    - thumbnail_image
    - creator
    - modified_at
    - url

--- src/types/NewsroomGallery.ts
+++ src/types/NewsroomGallery.ts
@@ -1,13 +1,22 @@
 import { GalleryStatus } from './GalleryStatus';
 import { NewsroomGalleryImage } from './NewsroomGalleryImage';
+import {UserRef} from "./UserRef";
+import {UploadedImage} from "@prezly/uploads";
 
 export interface NewsroomGallery {
     id: number;
     uuid: string;
+    name: string;
+    /** @deprecated Use `name` instead */
     title: string;
+    description: string | null;
+    thumbnail_image: UploadedImage | null;
+    url: string;
     status: GalleryStatus;
+    creator: UserRef | null;
     created_at: string;
     updated_at: string;
+    modified_at: string | null;
     uploadcare_group_uuid: string | null;
     content: string;
     images_number: number;
