b20788e1c687643a07acd67e7a709ad6df028f60
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-10-19 14:35:47 +0300

    Export `HeadersMap`

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from '@prezly/uploads';
 
 export { Client as PrezlyClient, createClient as createPrezlyClient } from './Client';
 
+export { HeadersMap } from './http';
 export * from './endpoints';
 export * from './types';
