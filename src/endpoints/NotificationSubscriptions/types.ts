e9178ab4a50fe12ceea5d998c50a13637f4afea0
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-11-03 12:12:56 +0200

    Drop BC-preserving `NotificationSubscriptionUpdateRequest` type

--- src/endpoints/NotificationSubscriptions/types.ts
+++ src/endpoints/NotificationSubscriptions/types.ts
@@ -3,9 +3,3 @@ import type { NotificationSubscription } from '../../types';
 export interface UpdateRequest {
     subscriptions: Pick<NotificationSubscription, 'notification_type' | 'is_email_subscribed'>[];
 }
-
-/**
- * @deprecated This is a BC alias to `UpdateRequest`. Please use `UpdateRequest` directly instead.
- * @see UpdateRequest
- */
-export type NotificationSubscriptionsUpdateRequest = UpdateRequest;
