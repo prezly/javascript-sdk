da3977ed7db46afee19089335a3e1f5655120dbd
Oleg Semyonov <oleg@prezly.com>
2022-11-08 18:34:42 +0400

    Fix typo

--- src/endpoints/NotificationSubscriptions/types.ts
+++ src/endpoints/NotificationSubscriptions/types.ts
@@ -2,7 +2,7 @@ import type { NotificationSubscription } from '../../types';
 
 interface SubscriptionUpdate {
     notification_type: NotificationSubscription.Type['id'];
-    is_email_subscribed: NotificationSubscription['is_email_required'];
+    is_email_subscribed: NotificationSubscription['is_email_subscribed'];
 }
 
 export interface UpdateRequest {
