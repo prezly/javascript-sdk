a6923dfa22b893acc0fcf2cb7bcf0aed10b9739a
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2023-01-10 11:56:50 +0100

    Define agency plan levels

--- src/types/Plan.ts
+++ src/types/Plan.ts
@@ -7,4 +7,8 @@ export enum PlanLevel {
     PREMIUM = 'premium',
     PROFESSIONAL = 'professional',
     STARTER = 'starter',
+    AGENCY_SMALL = 'agency_small',
+    AGENCY_MEDIUM = 'agency_medium',
+    AGENCY_LARGE = 'agency_large',
+    AGENCY_HUGE = 'agency_huge',
 }
