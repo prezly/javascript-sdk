9f485f3e960c56ca54fc49a5e50c608144c50606
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2023-02-22 13:33:37 +0100

    Support plan API

--- src/endpoints/Billing/Client.ts
+++ src/endpoints/Billing/Client.ts
@@ -2,6 +2,7 @@ import type { DeferredJobsApiClient } from '../../api';
 import { routing } from '../../routing';
 
 import type { SignupRequest, SignupResponse } from './types';
+import type { Plan } from "../../types";
 
 export class Client {
     private readonly apiClient: DeferredJobsApiClient;
@@ -13,5 +14,12 @@ export class Client {
     async signup(payload: SignupRequest): Promise<SignupResponse> {
         const url = `${routing.billing}/signup`;
         return this.apiClient.post<SignupResponse>(url, { payload });
+    }
+
+    async getPlan(): Promise<Plan> {
+        const { plan } = await this.apiClient.get<{ plan: Plan }>(
+            `${routing.billing}/plan`
+        );
+        return plan;
     }
 }
--- src/types/Plan.ts
+++ src/types/Plan.ts
@@ -1,3 +1,40 @@
+import type {Limit} from "../endpoints/PricingTables";
+
+export interface Plan {
+    display_name: string;
+    description: string | null;
+    is_legacy: boolean;
+    is_deprecated: boolean;
+    is_superior: boolean;
+    is_trial: boolean;
+    pricing_table_option_id: string | null;
+    billing_cycle: string;
+    currency: string;
+    ends_at: string | null;
+    discount: Discount | null;
+    usage: Usage[];
+    items: Item[];
+}
+
+export interface Discount {
+    amount_off: number;
+    percent_off: number;
+}
+
+export interface Usage {
+    limit: Limit;
+    used: number;
+}
+
+export interface Item {
+    amount: number;
+    quantity: number;
+    unit: string | null;
+    billing_cycle: string;
+    currency: string;
+}
+
+/** @deprecated Will be dropped in future */
 export enum PlanLevel {
     BASIC = 'basic',
     CORE = 'core',
