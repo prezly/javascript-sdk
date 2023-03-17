9c7b6abb3fdb0b09361b9ed42a2066bd9caca50b
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-03-17 16:29:46 +0200

    [DEV-10349] Declare `ContactDuplicateSuggestion` helper functions

--- src/types/ContactDuplicateSuggestion.ts
+++ src/types/ContactDuplicateSuggestion.ts
@@ -18,4 +18,37 @@ export namespace ContactDuplicateSuggestion {
         ACCEPTED = 'accepted',
         DECLINED = 'declined',
     }
+
+    export function isUnseen(suggestion: Pick<ContactDuplicateSuggestion, 'status'>): boolean;
+    export function isUnseen(status: ContactDuplicateSuggestion['status']): boolean;
+    export function isUnseen(
+        arg: Pick<ContactDuplicateSuggestion, 'status'> | ContactDuplicateSuggestion['status'],
+    ): boolean {
+        if (arg !== null && typeof arg === 'object') {
+            return arg.status === Status.UNSEEN;
+        }
+        return arg === Status.UNSEEN;
+    }
+
+    export function isAccepted(suggestion: Pick<ContactDuplicateSuggestion, 'status'>): boolean;
+    export function isAccepted(status: ContactDuplicateSuggestion['status']): boolean;
+    export function isAccepted(
+        arg: Pick<ContactDuplicateSuggestion, 'status'> | ContactDuplicateSuggestion['status'],
+    ): boolean {
+        if (arg !== null && typeof arg === 'object') {
+            return arg.status === Status.ACCEPTED;
+        }
+        return arg === Status.ACCEPTED;
+    }
+
+    export function isDeclined(suggestion: Pick<ContactDuplicateSuggestion, 'status'>): boolean;
+    export function isDeclined(status: ContactDuplicateSuggestion['status']): boolean;
+    export function isDeclined(
+        arg: Pick<ContactDuplicateSuggestion, 'status'> | ContactDuplicateSuggestion['status'],
+    ): boolean {
+        if (arg !== null && typeof arg === 'object') {
+            return arg.status === Status.DECLINED;
+        }
+        return arg === Status.DECLINED;
+    }
 }
