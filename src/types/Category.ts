f8d1772897ae7907826c371a4735461222353593
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-07 22:16:07 +0200

    [DEV-11945] Declare Category.Translation type

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -1,16 +1,11 @@
-import type { CultureRef } from './Culture';
+import type { Culture, CultureRef } from './Culture';
 
 export interface CategoryRef {
     id: number;
     display_name: string;
     display_description: string | null;
     i18n: {
-        [localeCode: CultureRef['code']]: {
-            description: string | null;
-            locale: CultureRef;
-            name: string;
-            slug: string | null;
-        };
+        [localeCode: Culture.Code]: Category.Translation;
     };
     stories_number: number;
 }
@@ -18,4 +13,13 @@ export interface CategoryRef {
 export interface Category extends CategoryRef {
     stories_number: number;
     public_stories_number: number;
+}
+
+export namespace Category {
+    export interface Translation {
+        locale: CultureRef;
+        slug: string | null;
+        name: string;
+        description: string | null;
+    }
 }
