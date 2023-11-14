ddb5a504e4751deb3f965592be948d5517faa671
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-14 19:52:38 +0200

    [DEV-11989] Introduce utilities to check and get Category translations

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -22,4 +22,83 @@ export namespace Category {
         name: string;
         description: string | null;
     }
+
+    export function isTranslated<T extends Category, LocaleCode extends Culture['code']>(
+        category: T,
+        locale: LocaleIdentifier<LocaleCode>,
+    ): category is WithNonEmptyTranslation<T, LocaleCode> {
+        const translation = category.i18n[localeCode(locale)];
+        return isNonEmpty(translation);
+    }
+
+    export function translation(
+        category: Category,
+        locale: LocaleIdentifier,
+    ): TranslatedCategory | undefined {
+        const [translation] = Category.translations(category, locale);
+        return translation;
+    }
+
+    export function translations(
+        category: Category,
+        locale?: LocaleIdentifier,
+    ): TranslatedCategory[];
+
+    export function translations(
+        categories: Category[],
+        locale?: LocaleIdentifier,
+    ): TranslatedCategory[];
+
+    export function translations(
+        input: Category | Category[],
+        locale?: LocaleIdentifier,
+    ): TranslatedCategory[] {
+        const code = locale ? localeCode(locale) : undefined;
+        const categories = Array.isArray(input) ? input : [input];
+
+        return categories.flatMap((category) => {
+            const translations = code ? [category.i18n[code]] : Object.values(category.i18n);
+
+            return translations.filter(isNonEmpty).map((translation) => {
+                return {
+                    id: category.id,
+                    locale: translation.locale.code,
+                    slug: translation.slug,
+                    name: translation.name,
+                    description: translation.description,
+                };
+            });
+        });
+    }
+}
+
+export interface TranslatedCategory {
+    id: Category['id'];
+    locale: Culture['code'];
+    slug: NonNullable<Category.Translation['slug']>;
+    name: Category.Translation['name'];
+    description: Category.Translation['description'];
+}
+
+type LocaleIdentifier<Locale extends Culture['code'] = Culture['code']> = Locale | { code: Locale };
+
+type NonEmptyTranslation = Category.Translation & {
+    slug: NonNullable<Category.Translation['slug']>;
+    name: Exclude<Category.Translation['name'], ''>;
+};
+
+type WithNonEmptyTranslation<T extends Category, LocaleCode extends Culture['code']> = T & {
+    i18n: {
+        [localeCode in LocaleCode]: NonEmptyTranslation;
+    };
+};
+
+function isNonEmpty(
+    translation: Category.Translation | undefined,
+): translation is NonEmptyTranslation {
+    return Boolean(translation?.name && translation?.slug);
+}
+
+function localeCode(locale: LocaleIdentifier): Culture['code'] {
+    return typeof locale === 'string' ? locale : locale.code;
 }
