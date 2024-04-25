51f885785c4727456263b664abbc9913755f65c1
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-04-25 16:17:20 +0200

    [DEV-12883] Extend the ContactTags model by new extra fields

--- src/types/ContactTag.ts
+++ src/types/ContactTag.ts
@@ -1,8 +1,16 @@
+import type { UserRef } from './User';
+
+type Iso8601DateTime = string;
+
 export interface ContactTag {
     id: number;
     name: string;
     contacts_number: number;
     contacts_url: string;
+    created_at: Iso8601DateTime | undefined; // May be undefined for older tags
+    updated_at: Iso8601DateTime | undefined; // May be undefined for older tags
+    creator: UserRef | undefined; // May be undefined for older tags
+    last_updated_by_user: UserRef | undefined; // May be undefined for older tags
 }
 
 export namespace ContactTag {
