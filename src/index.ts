e8e129fc2a89aefc77f4e392751f1c03767d9233
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-04 13:35:46 +0700

    Export api routing constant

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from '@prezly/uploads';
 export { type Client as PrezlyClient, createClient as createPrezlyClient } from './Client';
 
 export { type HeadersMap, ApiError } from './http';
+export { routing } from './routing';
 export * from './endpoints';
 export * from './types';
