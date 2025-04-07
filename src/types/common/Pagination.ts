9c88a6cfcfe27f4d95c4cf7932ae4ee28573beb9
kudlajz <kudlajzzz@gmail.com>
2025-04-07 14:50:50 +0200

    Add offset and limit to Pagination

--- src/types/common/Pagination.ts
+++ src/types/common/Pagination.ts
@@ -2,7 +2,9 @@
  * @see https://api.prezly.com/v2/common/schema.json#/definitions/Pagination
  */
 export interface Pagination {
+    limit: number;
     matched_records_number: number;
+    offset: number;
     total_records_number: number;
 }
 
