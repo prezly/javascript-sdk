fe828fb76c8f16d9c9255dc4069dbc7e0921094c
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2025-05-21 11:03:34 +0200

    Newsroom conversion SDK

--- src/endpoints/Newsrooms/Client.ts
+++ src/endpoints/Newsrooms/Client.ts
@@ -100,6 +100,13 @@ export function createClient(api: DeferredJobsApiClient) {
         return newsroom;
     }
 
+    async function convert(id: NewsroomId): Promise<Newsroom> {
+        const { newsroom } = await api.post<{ newsroom: Newsroom }>(
+            `${routing.newsroomsUrl}/${id}/convert`,
+        );
+        return newsroom;
+    }
+
     return {
         list,
         search,
@@ -111,5 +118,6 @@ export function createClient(api: DeferredJobsApiClient) {
         delete: doDelete,
         takeOnline,
         takeOffline,
+        convert,
     };
 }
