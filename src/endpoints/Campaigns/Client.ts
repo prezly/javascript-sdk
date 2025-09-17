f6c40132834ae68e58742e75b4efdda69dd53f3b
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2025-09-17 15:06:25 +0200

    Fix code style

--- src/endpoints/Campaigns/Client.ts
+++ src/endpoints/Campaigns/Client.ts
@@ -12,7 +12,8 @@ import type {
     CampaignResponse,
     UpdateRequest,
     ListResponse,
-    SearchOptions, DuplicateOptions,
+    SearchOptions,
+    DuplicateOptions,
 } from './types';
 
 export type Client = ReturnType<typeof createClient>;
@@ -48,8 +49,13 @@ export function createClient(api: DeferredJobsApiClient) {
         return api.get<CampaignResponse>(`${routing.campaignsUrl}/${id}`);
     }
 
-    async function duplicate(id: Campaign['id'], options?: DuplicateOptions): Promise<CampaignResponse> {
-        return api.post<CampaignResponse>(`${routing.campaignsUrl}/${id}/duplicate`, { payload: options });
+    async function duplicate(
+        id: Campaign['id'],
+        options?: DuplicateOptions,
+    ): Promise<CampaignResponse> {
+        return api.post<CampaignResponse>(`${routing.campaignsUrl}/${id}/duplicate`, {
+            payload: options,
+        });
     }
 
     async function doDelete(id: Campaign['id']): Promise<void> {
